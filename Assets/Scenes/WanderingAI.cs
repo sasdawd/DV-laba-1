7ab16c7822c7b2e29c787152014992e257b1769e
Sashok <sasha.isanov.03@mail.ru>
2022-03-10 16:55:21 +0300

    Begin

---
+++ Assets/Scenes/WanderingAI.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+    private bool _alive;
+    void Start()
+    {
+        _alive = true;
+    }
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+    void Update()
+    {
+        if (_alive == true)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if(Physics.SphereCast(ray,0.75f, out hit))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+            if (hitObject.GetComponent<PlayerCharacter>())
+            {
+                if(_fireball == null)
+                {
+                    _fireball = Instantiate(fireballPrefab) as GameObject;
+                    _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                    _fireball.transform.rotation = transform.rotation;
+                }
+            }
+            else if (hit.distance < obstacleRange)
+            {
+                float angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+            }
+        }    
+    }
+
+    public void SetAlive(bool alive)
+    {
+        _alive = alive;
+    }
+}
